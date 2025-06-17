638779ffd78469a3c1ab150156b99a6f64cce9cf
ellasmith2090 <ellasmith200900@gmail.com>
2025-06-17 18:58:46 +0100

    Update main.js to fix Shoelace imports

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
 //main.js
 
+import "@shoelace-style/shoelace/dist/themes/light.css";
 import "@shoelace-style/shoelace/dist/shoelace.js";
 import "./styles/main.scss";
 import App from "./App.js";
 
-// Clear rootEl and log
 console.log("[main.js] Clearing root element");
-document.getElementById("root").innerHTML = "";
+const rootEl = document.getElementById("root");
+rootEl.innerHTML = "";
 
-// Initialize the app
 console.log("[main.js] Initializing App");
 App.init().catch((err) => {
-  document.getElementById("root").innerHTML = `
-    <p style="color:red;">App failed to load: ${err.message}</p>
-  `;
+  rootEl.innerHTML = `<p style="color:red;">App failed to load: ${err.message}</p>`;
   console.error("[main.js] App init failed:", err);
 });
